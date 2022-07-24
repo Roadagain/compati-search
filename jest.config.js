abf43e3fe91ac71278111b591f5e9cb14bae28c5
Roadagain <ringoh72@gmail.com>
2022-07-24 19:01:35 +0900

    コメントアウトした部分を削除

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,6 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
   verbose: true,
-  // preset: 'ts-jest/presets/default-esm',
-  // globals: {
-  //   'ts-jest': {
-  //     useESM: true,
-  //   },
-  // },
-  // moduleNameMapper: {
-  //   '^(\\.{1,2}/.*)\\.js$': '$1',
-  // },
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest'
   }
