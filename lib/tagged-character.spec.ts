22670dc2210d915f88c8deabd4458cc81fbd652e
Roadagain <ringoh72@gmail.com>
2022-07-24 22:45:01 +0900

    複数タグでの検索関数を実装

--- lib/tagged-character.spec.ts
+++ lib/tagged-character.spec.ts
@@ -1,4 +1,8 @@
-import { searchCharactersByTag, TaggedCharacter } from './tagged-character';
+import {
+  searchCharactersByMultipleTags,
+  searchCharactersByTag,
+  TaggedCharacter,
+} from './tagged-character';
 
 describe('searchCharactersByTag', () => {
   const testTag = 'test-tag';
@@ -27,7 +31,66 @@ describe('searchCharactersByTag', () => {
 
   describe('指定したタグを持つキャラクターがいない場合', () => {
     it('空配列が返る', () => {
-      expect(searchCharactersByTag(characters, 'not-set-tag')).toStrictEqual([]);
+      expect(searchCharactersByTag(characters, 'not-set-tag')).toStrictEqual(
+        []
+      );
+    });
+  });
+});
+
+describe('searchCharactersByMultipleTags', () => {
+  const testTag1 = 'x-ray';
+  const testTag2 = 'yankee';
+  const testTag3 = 'zulu';
+  const alpha: TaggedCharacter = {
+    name: 'alpha',
+    tags: new Set([testTag1, 'other-tag', testTag2]),
+  };
+  const beta: TaggedCharacter = {
+    name: 'beta',
+    tags: new Set(['other-tag', testTag1]),
+  };
+  const gamma: TaggedCharacter = {
+    name: 'gamma',
+    tags: new Set([testTag3, testTag2, testTag1]),
+  };
+  const delta: TaggedCharacter = {
+    name: 'delta',
+    tags: new Set(['other-tag']),
+  };
+  const characters = [alpha, beta, gamma, delta];
+
+  describe('複数タグがヒットする場合', () => {
+    it('ヒットしたタグが多い順にキャラクターの配列が返る', () => {
+      expect(
+        searchCharactersByMultipleTags(characters, [
+          testTag1,
+          testTag2,
+          testTag3,
+        ])
+      ).toStrictEqual([gamma, alpha, beta]);
+    });
+  });
+
+  describe('指定タグが1つの場合', () => {
+    it('searchCharactersByTagと結果が一致する', () => {
+      expect(
+        searchCharactersByMultipleTags(characters, [testTag1])
+      ).toStrictEqual(searchCharactersByTag(characters, testTag1));
+    });
+  });
+
+  describe('指定したタグを持つキャラクターがいない場合', () => {
+    it('空配列が返る', () => {
+      expect(
+        searchCharactersByMultipleTags(characters, ['not-set-tag'])
+      ).toStrictEqual([]);
+    });
+  });
+
+  describe('タグが指定されない場合', () => {
+    it('空配列が返る', () => {
+      expect(searchCharactersByMultipleTags(characters, [])).toStrictEqual([]);
     });
   });
 });
--- lib/tagged-character.ts
+++ lib/tagged-character.ts
@@ -13,3 +13,22 @@ export const searchCharactersByTag = (
     return character.tags.has(tag);
   });
 };
+
+// 複数タグ検索
+// 一致数が多い順に返す
+export const searchCharactersByMultipleTags = (
+  characters: TaggedCharacter[],
+  tags: Tag[]
+): TaggedCharacter[] => {
+  const matchedTagsCounts = characters.flatMap((character) => {
+    const matchedCount = tags.filter((tag) => character.tags.has(tag)).length;
+    return matchedCount > 0
+      ? {
+          character,
+          count: matchedCount,
+        }
+      : [];
+  });
+  matchedTagsCounts.sort((a, b) => b.count - a.count);
+  return matchedTagsCounts.map(({ character }) => character);
+};
