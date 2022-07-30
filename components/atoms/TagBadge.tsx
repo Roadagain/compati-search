6f48896c7cb01948365e3f6180c0f94b53f5912b
Roadagain <ringoh72@gmail.com>
2022-07-30 20:58:53 +0900

    タグ表示用のコンポーネントを作成

---
+++ components/atoms/TagBadge.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+interface Props {
+  /**
+   * タグ
+   */
+  children: string;
+}
+
+export const TagBadge: React.FC<Props> = ({ children }) => (
+  <Button
+    size="small"
+    variant="outlined"
+    sx={{ display: 'inline-block', textTransform: 'none' }}
+  >
+    {children}
+  </Button>
+);
---
+++ stories/atoms/TagBadge.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { TagBadge } from '../../components/atoms/TagBadge';
+
+const componentMeta: ComponentMeta<typeof TagBadge> = {
+  title: 'Atoms/TagBadge',
+  component: TagBadge,
+  argTypes: {
+    children: { control: 'text' },
+  },
+};
+export default componentMeta;
+
+const Template: ComponentStory<typeof TagBadge> = (args) => (
+  <TagBadge {...args} />
+);
+
+export const Badge = Template.bind({});
+Badge.args = {
+  children: 'タグ',
+};
