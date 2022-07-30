e189b0855f03ed32ef056fbf4641f87002ce654a
Roadagain <ringoh72@gmail.com>
2022-07-30 22:33:20 +0900

    検索画面に今の検索条件を表示する

--- components/organisms/CharactersSearcher.tsx
+++ components/organisms/CharactersSearcher.tsx
@@ -9,6 +9,7 @@ import { CharacterCard } from '../molecules/CharacterCard';
 import { SearchForm } from '../molecules/SearchForm';
 import Box from '@mui/material/Box';
 import { SearchTarget } from '../molecules/SearchTargetSelect';
+import { SearchCondition } from '../molecules/SearchCondition';
 
 interface Props {
   /**
@@ -17,10 +18,17 @@ interface Props {
   characters: TaggedCharacter[];
 }
 
+interface SearchCondition {
+  target: SearchTarget;
+  text: string;
+}
+
 export const CharactersSearcher: React.FC<Props> = ({ characters }) => {
   const [searchResults, setSearchResults] = React.useState<TaggedCharacter[]>(
     []
   );
+  const [searchCondition, setSearchCondition] =
+    React.useState<SearchCondition | null>(null);
   const search = (text: string, target: SearchTarget) => {
     const words = text.split(/\s/);
     const searchResults =
@@ -28,11 +36,20 @@ export const CharactersSearcher: React.FC<Props> = ({ characters }) => {
         ? filterCharactersByTags(characters, words)
         : filterCharactersByNameWords(characters, words);
     setSearchResults(searchResults);
+    setSearchCondition({
+      target,
+      text,
+    });
   };
 
   return (
     <Box>
       <SearchForm onSearch={search} />
+      {searchCondition ? (
+        <Box mt={2}>
+          <SearchCondition {...searchCondition} />
+        </Box>
+      ) : null}
       <Grid container spacing={2} sx={{ mt: 1 }}>
         {searchResults.map(({ name, tags }) => (
           <Grid item key={name} xs={12} sm={6} md={4}>
