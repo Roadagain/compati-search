9ca874f64c79ff21f4580f3afec6eca8eec83614
Roadagain <ringoh72@gmail.com>
2022-07-30 20:57:54 +0900

    CssBaseline置いてなくてstorybookと見た目がちょっと食い違ってた

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,12 +6,13 @@ import '@fontsource/noto-sans-jp/400.css';
 import '@fontsource/noto-sans-jp/500.css';
 import '@fontsource/noto-sans-jp/700.css';
 import '@fontsource/noto-sans-jp/900.css';
-import { ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from '../styles/theme';
 
 const AppLayout = ({ Component, pageProps }: AppProps) => (
   <main>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
   </main>
