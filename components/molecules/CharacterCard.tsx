c7011c5fcfc3277673e83ee3c920d748b6abd473
Roadagain <ringoh72@gmail.com>
2022-07-30 20:59:12 +0900

    検索結果のキャラクター表示にタグの一覧も出す

--- components/molecules/CharacterCard.tsx
+++ components/molecules/CharacterCard.tsx
@@ -1,11 +1,24 @@
-import { Card, CardHeader } from '@mui/material';
+import { Card, CardContent, CardHeader } from '@mui/material';
 import React from 'react';
 import { TaggedCharacter } from '../../lib/tagged-character';
+import { TagBadge } from '../atoms/TagBadge';
+import Stack from '@mui/material/Stack';
 
 type Props = TaggedCharacter;
 
-export const CharacterCard: React.FC<Props> = ({ name }) => (
+export const CharacterCard: React.FC<Props> = ({ name, tags }) => (
   <Card elevation={2}>
     <CardHeader title={name} />
+    <CardContent sx={{ overflowX: 'scroll' }}>
+      <Stack
+        direction="row"
+        spacing={1}
+        sx={{ display: 'inline-block', whiteSpace: 'nowrap' }}
+      >
+        {tags.map((tag) => (
+          <TagBadge>{tag}</TagBadge>
+        ))}
+      </Stack>
+    </CardContent>
   </Card>
 );
