49e8fd3469402d156c2dbe69b9aacfa24518bfd7
Roadagain <ringoh72@gmail.com>
2022-07-30 10:50:18 +0900

    MUIのサンプルを置く

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { loadSampleCharactersData } from '../lib/load-data';
+import { SampleButton } from '../components/atoms/SampleButton';
 
 const Index: React.FC = () => {
   React.useEffect(() => {
     console.log('characters data', loadSampleCharactersData());
   });
-  return <main>character-tag-searcher</main>;
+  return (
+    <main>
+      <p>character-tag-searcher</p>
+      <SampleButton>ボタン</SampleButton>
+    </main>
+  );
 };
 
 export default Index;
