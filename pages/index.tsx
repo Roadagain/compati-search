f691e3a64d1520d30c065f04eadf40ad6f72f477
Roadagain <ringoh72@gmail.com>
2022-08-11 02:38:18 +0900

    クエリを見てキャラデータをfetchできるようにする

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,50 @@
 import React from 'react';
-import { loadSampleCharactersData } from '../lib/load-data';
+import {
+  loadCharactersDataFromJson,
+  loadSampleCharactersData,
+} from '../lib/load-data';
 import { Box, Container, Typography } from '@mui/material';
 import { CharactersSearcher } from '../components/organisms/CharactersSearcher';
 import { TaggedCharacter } from '../lib/tagged-character';
-import { GetStaticProps, NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
 
 interface Props {
-  characters: TaggedCharacter[]
+  characters: TaggedCharacter[];
 }
 
 const Index: NextPage<Props> = ({ characters }) => (
-    <Container>
-      <Box sx={{ p: 1 }}>
-        <Typography variant="h5">互換キャラサーチ</Typography>
-      </Box>
-      <CharactersSearcher characters={characters} />
-    </Container>
-  );
+  <Container>
+    <Box sx={{ p: 1 }}>
+      <Typography variant="h5">互換キャラサーチ</Typography>
+    </Box>
+    <CharactersSearcher characters={characters} />
+  </Container>
+);
 
-export const getStaticProps: GetStaticProps<Props> = () => {
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  const dataName = context.query.data;
+  if (!dataName || dataName === 'sample') {
+    return {
+      props: {
+        characters: loadSampleCharactersData(),
+      },
+    };
+  }
+
+  const result = await fetch(
+    `${process.env.CHARACTERS_DATA_STORAGE_URL}/${dataName}.json?alt=media`,
+    {
+      method: 'GET'
+    }
+  );
+  const json = await result.json();
   return {
     props: {
-      characters: loadSampleCharactersData()
-    }
-  }
-}
+      characters: loadCharactersDataFromJson(json),
+    },
+  };
+};
 
 export default Index;
