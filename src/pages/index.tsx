5a3751b50603e524344abbced0b19268820f7974
Roadagain <ringoh72@gmail.com>
2022-08-14 11:22:34 +0900

    indexの実装を簡潔にする

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,6 @@
-import {
-  loadCharactersDataFromJson,
-  loadSampleCharactersData,
-} from '../lib/load-data';
-import { GetServerSideProps } from 'next';
-import { Props, SearchTemplate } from '../components/templates/SearchTemplate';
+import { loadSampleCharactersData } from '../lib/load-data';
+import { SearchTemplate } from '../components/templates/SearchTemplate';
 
-export default SearchTemplate;
+const Index = () => <SearchTemplate characters={loadSampleCharactersData()} />;
 
-export const getServerSideProps: GetServerSideProps<Props> = async (
-  context
-) => {
-  const dataName = context.query.data;
-  if (!dataName || dataName === 'sample') {
-    return {
-      props: {
-        characters: loadSampleCharactersData(),
-      },
-    };
-  }
-
-  const result = await fetch(
-    `${process.env.CHARACTERS_DATA_STORAGE_URL}/${dataName}.json?alt=media`
-  );
-  const json = await result.json();
-  return {
-    props: {
-      characters: loadCharactersDataFromJson(json),
-    },
-  };
-};
+export default Index;
