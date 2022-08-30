2ff767f32c2fe02e7d9d04f03278e1bd81d3e125
Roadagain <ringoh72@gmail.com>
2022-08-31 00:40:17 +0900

    データ取得時のクエリを削る

--- src/pages/api/characters-data/[name].ts
+++ src/pages/api/characters-data/[name].ts
@@ -6,7 +6,7 @@ const handler: NextApiHandler = async (request, response) => {
     return response.status(400).json({ error: 'Name must be string' });
   }
   const fetchResult = await fetch(
-    `${process.env.CHARACTERS_DATA_STORAGE_URL}/${name}.json?alt=media`
+    `${process.env.CHARACTERS_DATA_STORAGE_URL}/${name}.json`
   );
   if (fetchResult.status === 404) {
     return response.status(404).json({ error: 'Not Found' });
