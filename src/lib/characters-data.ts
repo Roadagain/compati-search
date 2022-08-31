910d2d4ca1ac26009ae1bda1298f19a2be1837b0
Roadagain <ringoh72@gmail.com>
2022-09-01 01:11:38 +0900

    キャラデータ全体の型とtype guardを定義

---
+++ src/lib/characters-data.ts
@@ -0,0 +1,19 @@
+import { isTaggedCharacter } from './load-data';
+import { isMetadata, Metadata } from './metadata';
+import { TaggedCharacter } from './tagged-character';
+
+export interface CharactersData {
+  characters: TaggedCharacter[];
+  metadata: Metadata;
+}
+
+type WouldBeCharactersData = { [K in keyof CharactersData]?: unknown };
+
+export const isCharactersData = (
+  obj: WouldBeCharactersData
+): obj is CharactersData => {
+  if (!Array.isArray(obj.characters) || typeof obj.metadata === 'undefined') {
+    return false;
+  }
+  return obj.characters.every(isTaggedCharacter) && isMetadata(obj.metadata);
+};
