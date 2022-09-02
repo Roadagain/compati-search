b5299287abe02e7418ec3f07c117dd9ecf34b2e2
Roadagain <ringoh72@gmail.com>
2022-09-02 23:20:13 +0900

    全キャラ表示スイッチにsxを指定できるようにする

--- src/components/molecules/ShowAllCharactersSwitch.tsx
+++ src/components/molecules/ShowAllCharactersSwitch.tsx
@@ -1,4 +1,4 @@
-import { FormControlLabel, Switch } from '@mui/material';
+import { FormControlLabel, Switch, SxProps, Theme } from '@mui/material';
 import React from 'react';
 
 interface Props {
@@ -14,12 +14,17 @@ interface Props {
    * スイッチ時のハンドラ
    */
   onChange: (checked: boolean) => void;
+  /**
+   * テーマ関連のスタイル指定
+   */
+  sx?: SxProps<Theme>;
 }
 
 export const ShowAllCharactersSwitch: React.FC<Props> = ({
   checked,
   character,
   onChange,
+  sx,
 }) => {
   const onChangeSwitch: React.ChangeEventHandler<HTMLInputElement> =
     React.useCallback(
@@ -33,6 +38,7 @@ export const ShowAllCharactersSwitch: React.FC<Props> = ({
     <FormControlLabel
       control={<Switch checked={checked} onChange={onChangeSwitch} />}
       label={`全${character}を表示`}
+      sx={sx}
     />
   );
 };
