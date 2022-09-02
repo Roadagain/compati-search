abadc2aaa57c6eed3cd678c4ccfbd88968491034
Roadagain <ringoh72@gmail.com>
2022-09-03 04:22:48 +0900

    メタデータもflux stateに入れる

--- src/flux/state.ts
+++ src/flux/state.ts
@@ -1,3 +1,4 @@
+import { Metadata } from '../lib/metadata';
 import { SearchTarget } from '../lib/search-target';
 import { TaggedCharacter } from '../lib/tagged-character';
 
@@ -10,11 +11,15 @@ interface SearchState {
 
 export interface State {
   characters: TaggedCharacter[];
+  metadata: Metadata;
   search: SearchState;
 }
 
 export const initialState: State = {
   characters: [],
+  metadata: {
+    character: 'キャラクター',
+  },
   search: {
     target: SearchTarget.TAG,
     words: [],
