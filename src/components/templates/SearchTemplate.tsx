091774ccb7815524e15ed6aa22f60df87ca76ac3
Roadagain <ringoh72@gmail.com>
2022-09-03 05:16:18 +0900

    データフェッチを関数に切り分ける

--- src/components/templates/SearchTemplate.tsx
+++ src/components/templates/SearchTemplate.tsx
@@ -4,7 +4,7 @@ import { FluxContext } from '../../flux/context';
 import { SearchForm } from '../organisms/SearchForm';
 import { SearchCondition } from '../organisms/SearchCondition';
 import { SearchResults } from '../organisms/SearchResults';
-import { loadCharactersDataFromJson } from '../../lib/load-data';
+import { fetchCharactersData } from '../../lib/fetch-data';
 
 export interface Props {
   /**
@@ -16,14 +16,7 @@ export interface Props {
 export const SearchTemplate: React.FC<Props> = ({ dataName }) => {
   const { state, dispatch } = React.useContext(FluxContext);
   React.useEffect(() => {
-    fetch(`/api/characters-data/${dataName}`)
-      .then((res) => {
-        if (res.status === 404) {
-          throw new Error('Not Found');
-        }
-        return res.json();
-      })
-      .then(loadCharactersDataFromJson)
+    fetchCharactersData(dataName)
       .then((charactersData) => {
         dispatch({
           type: 'load-characters-data',
