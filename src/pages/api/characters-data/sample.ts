5f1c81a0a6078fa5a896f7adbdf9111a4763199c
Roadagain <ringoh72@gmail.com>
2022-11-05 17:18:04 +0900

    APIを使わなくなったのでルーティングを削除

--- src/pages/api/characters-data/sample.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiHandler } from 'next';
-
-import sampleCharactersData from '../../../../sample/characters-data/sample.json';
-
-const handler: NextApiHandler = (_, response) => {
-  response.status(200).json(sampleCharactersData);
-};
-
-export default handler;
