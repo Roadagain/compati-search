45225769a0b205bc93304034e1a2351a6778261d
Roadagain <ringoh72@gmail.com>
2022-11-06 02:08:14 +0900

    検索対象にカテゴリを含めて指定できるようにする

--- src/lib/search-target.ts
+++ src/lib/search-target.ts
@@ -2,3 +2,14 @@ export enum SearchType {
   TAG,
   NAME,
 }
+
+interface SearchTargetSimple {
+  type: SearchType;
+}
+
+interface SearchTargetTagCategory {
+  type: SearchType.TAG;
+  category: string;
+}
+
+export type SearchTarget = SearchTargetSimple | SearchTargetTagCategory;
