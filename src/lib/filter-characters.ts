7bee6d8a8677a4220b4972a92fd6693b5cb1be20
Roadagain <ringoh72@gmail.com>
2022-11-13 02:40:29 +0900

    不要になったキャラフィルタ関数を削除

--- src/lib/filter-characters.ts
+++ src/lib/filter-characters.ts
@@ -1,42 +1,5 @@
 import { Tag, TaggedCharacter } from './tagged-character';
 
-export const filterCharactersByTagLabels = (
-  characters: TaggedCharacter[],
-  tagLabels: string[],
-  showAll: boolean
-): TaggedCharacter[] => {
-  return characters.filter((character) => {
-    const characterTagLabels = character.tags.map(({ label }) => label);
-    return (
-      (showAll || character.showDefault) &&
-      tagLabels.every((tagLabel) => {
-        const isMinus = tagLabel.startsWith('-');
-        const hasTag = characterTagLabels.includes(
-          tagLabel.slice(isMinus ? 1 : 0)
-        );
-        return isMinus ? !hasTag : hasTag;
-      })
-    );
-  });
-};
-
-export const filterCharactersByNameWords = (
-  characters: TaggedCharacter[],
-  nameWords: string[],
-  showAll: boolean
-): TaggedCharacter[] => {
-  return characters.filter(({ name, showDefault }) => {
-    return (
-      (showAll || showDefault) &&
-      nameWords.every((word) => {
-        const isMinus = word.startsWith('-');
-        const hasWord = name.includes(word.slice(isMinus ? 1 : 0));
-        return isMinus ? !hasWord : hasWord;
-      })
-    );
-  });
-};
-
 export const matchesNameWords = (name: string, words: string[]): boolean => {
   return words.every((word) => {
     const isMinus = word.startsWith('-');
