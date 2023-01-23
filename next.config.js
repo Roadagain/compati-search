2c5f1ebfd74dcc480a2b67eac09b922254985200
Roadagain <ringoh72@gmail.com>
2023-01-23 22:24:55 +0900

    /sampleのリダイレクトを削除する

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/sample',
-        destination: '/',
-        permanent: true,
-      },
-    ];
-  },
   experimental: {
     appDir: true,
   },
