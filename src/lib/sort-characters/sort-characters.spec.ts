0dc68fcb0e59828280a5ccb4111c800efa940fbb
Roadagain <ringoh72@gmail.com>
2023-01-27 01:28:21 +0900

    ソートのラッパーのテストを追加

---
+++ src/lib/sort-characters/sort-characters.spec.ts
@@ -0,0 +1,30 @@
+import { sortCharacters, SortOrder } from '.';
+import { sortById } from './id';
+import { sortByKana } from './kana';
+
+jest.mock('./id');
+jest.mock('./kana');
+describe('sortCharacters', () => {
+  describe.each`
+    order             | orderStr  | sortFunction
+    ${SortOrder.ID}   | ${'ID'}   | ${sortById}
+    ${SortOrder.KANA} | ${'かな'} | ${sortByKana}
+  `('ソート順が$orderStrのとき', ({ order, sortFunction }) => {
+    beforeEach(() => {
+      (sortFunction as unknown as jest.Mock).mockReturnValue([]);
+      sortCharacters([], order);
+    });
+
+    it(`${sortFunction.name}が呼ばれる`, () => {
+      expect(sortFunction).toBeCalledWith([]);
+    });
+  });
+
+  describe('ソート順の指定が既存のものと一致しないとき', () => {
+    it('例外を投げる', () => {
+      expect(() =>
+        sortCharacters([], 'unknown' as unknown as SortOrder)
+      ).toThrowError('Invalid sort order');
+    });
+  });
+});
