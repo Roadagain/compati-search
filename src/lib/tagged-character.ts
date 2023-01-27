b4e497abd5f71f2f5eca3e4255a748b3892219d8
Roadagain <ringoh72@gmail.com>
2023-01-28 02:30:06 +0900

    各種type guardを削除

--- src/lib/tagged-character.ts
+++ src/lib/tagged-character.ts
@@ -3,12 +3,6 @@ export interface Tag {
   label: string;
 }
 
-export type WouldBeTag = { [K in keyof Tag]?: unknown };
-
-export const isTag = (obj: WouldBeTag): obj is Tag => {
-  return typeof obj.category === 'string' && typeof obj.label === 'string';
-};
-
 export interface TaggedCharacter {
   id: number;
   name: string;
@@ -16,18 +10,3 @@ export interface TaggedCharacter {
   tags: Tag[];
   showDefault: boolean;
 }
-
-export type WouldBeTaggedCharacter = { [K in keyof TaggedCharacter]?: unknown };
-
-export const isTaggedCharacter = (
-  obj: WouldBeTaggedCharacter
-): obj is TaggedCharacter => {
-  return (
-    typeof obj.id === 'number' &&
-    typeof obj.name === 'string' &&
-    typeof obj.kana === 'string' &&
-    Array.isArray(obj.tags) &&
-    obj.tags.every(isTag) &&
-    typeof obj.showDefault === 'boolean'
-  );
-};
