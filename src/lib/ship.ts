2090f66f55458d0c30f71645481b71c83c67df46
Roadagain <ringoh72@gmail.com>
2023-02-18 04:50:24 +0900

    新しく艦船用の型を作る

--- src/lib/ship.ts
+++ src/lib/ship.ts
@@ -10,3 +10,16 @@ export interface Ship {
   tags: Tag[];
   showDefault: boolean;
 }
+
+export interface NewShip {
+  id: number;
+  name: string;
+  kana: string;
+  category: string;
+  type: string;
+  speed: string;
+  range: string;
+  equipments: string[];
+  abilities: string[];
+  showDefault: boolean;
+}
